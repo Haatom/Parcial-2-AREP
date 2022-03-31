96fbfcf269c77a04ccbcc813bc71743a4ec99f95
Haatom <goldom1730@gmail.com>
2022-03-30 20:54:24 -0500

    se añade a ec2

--- src/main/java/edu/escuelaing/parcial/App.java
+++ src/main/java/edu/escuelaing/parcial/App.java
@@ -1,48 +1,28 @@
 package edu.escuelaing.parcial;
-package com.parcial.arep;
-import static spark.Spark.*;
+
+import org.json.simple.JSONObject;
 import spark.Request;
 import spark.Response;
-import static spark.Spark.port;
 import static spark.Spark.*;
 
 public class App {
+    private static Calculadora calcular = new Calculadora();
     public static void main(String[] args) {
         port(getPort());
-        get("/ln", (req, res) -> primeraOperacion(req, res));
-        get("/atan", (req, res) -> segundaOperacion(req, res));
-
-
-    }
-    private static String primeraOperacion(Request req, Response res) {
-
-        Double num = Double.parseDouble(req.queryParams("value"));
-        Calculadora calculadora = new Calculadora();
-        res.type("application/json");
-        String pageContent=
-
-                "{"+
-                        " \"operation\": \"ln\","+"\n"+
-                        "\"input\":"+ num +"\n"+
-                        " \"output\":" +calculadora.calcularLn(num) +
-                        "}";
-        return pageContent;
+        get("/atan", (req, res) -> calculateData(req, res,"atan"));
+        get("/ln", (req, res) -> calculateData(req, res,"ln"));
     }
 
-    private static String segundaOperacion(Request req, Response res) {
-        Double num = Double.parseDouble(req.queryParams("value"));
-        Calculadora calculadora = new Calculadora();
-        res.type("application/json");
-        String pageContent=
-                "{"+
-                        " \"operation\": \"atan\","+"\n"+
-                        "\"input\":"+ num +"\n"+
-                        " \"output\":" +calculadora.calcularATan(num) +
-                        "}";
-        return pageContent;
+    private static JSONObject calculateData(Request req, Response res,String operation) {
+        double number = Double.parseDouble(req.queryParams("value"));
+        JSONObject obj = new JSONObject();
+        obj.put("input",number);
+        obj.put("operation",operation);
+        obj.put("output",(operation.equals("atan"))? calcular.calcularATan(number) : calcular.calcularLn(number));
+        return obj;
     }
 
-    private static int getPort() {
+    static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
